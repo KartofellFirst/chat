b990fb60e806d1fa0ddca594b2c6a9d3c4ae5211
KartofellFirst <deznevnikita8@gmail.com>
2025-10-21 23:23:09 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,19 @@
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('static-cache-v1').then(cache => {
+      return cache.addAll([
+        '/',
+        '/icon-192.png',
+        '/icon-512.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
